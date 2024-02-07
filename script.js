0b9e9262e0b22c7585179a5876deedf1bf99c03d
Gustavo Costa <117370951+Gucostaa@users.noreply.github.com>
2024-02-06 22:02:29 -0300

    JavaScript - Scroll To Top v1.0

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 
-function clickEvent() {
-    const click = document.getElementById("click")
-    click.innerHTML = "<li></li>"
+//Get the button
+let mybutton = document.getElementById("btn-back-to-top");
 
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 1500 ||
+    document.documentElement.scrollTop > 1500
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
